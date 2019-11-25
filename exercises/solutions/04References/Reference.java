633512c6ccb156971285c9426dc2451663cf2c2a
2xlink <2xlink@web.de>
2019-11-25 10:44:05 +0100

    Removes badSwap

--- exercises/solutions/04References/Reference.java
+++ exercises/solutions/04References/Reference.java
@@ -10,17 +10,5 @@ public class Reference {
 		Number ref2 = ref1;
 		ref1.number = 8;
 		System.out.println(ref2.number);
-
-		Number ref3 = new Number(4);
-		Number ref4 = new Number(6);
-		badSwap(ref3, ref4);
-		System.out.println(ref3.number);
-		System.out.println(ref4.number);
-    }
-
-    private static void badSwap(Object obj1, Object obj2) {
-		Object temp = obj1;
-		obj1 = obj2;
-		obj2 = temp;
 	}
 }
